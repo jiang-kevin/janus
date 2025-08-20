d497aa53a69e903649f73df53dc0282a621e4495
Kevin Jiang <kevin@kevinjiang.dev>
2025-08-20 09:43:47 -0400

    Add basic get/post restaurants

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel import Session
 
 from database import Database
 from models import Restaurant
@@ -13,13 +15,19 @@ async def lifespan(app: FastAPI):
     db.create()
     yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, root_path="/api/v1")
+SessionDep = Annotated[Session, Depends(db.session)]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
+@app.get("/restaurants")
+async def get_restaurants(session: SessionDep):
+    return session.query(Restaurant).all()
 
 @app.get("/restaurants/{name}")
-async def get_restaurant(name: str):
-    return db.session()
+async def get_restaurant_by_id(name: str, session: SessionDep):
+    return session.get(Restaurant, name)
+
+@app.post("/restaurants")
+async def create_restaurant(restaurant: Restaurant, session: SessionDep):
+    session.add(restaurant)
+    session.commit()
